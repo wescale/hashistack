f2ad9ab3ace34d9bf4e040b1868d37bff47756ca
BenjaminVouillaumeJT <105225272+BenjaminVouillaumeJT@users.noreply.github.com>
2023-01-18 16:24:42 +0100

    WIP (#6)
    * WIP
    
    * functionnal molecule test
    
    Co-authored-by: Benjamin Vouillaume <benjamin.vouillaume@wescale.fr>

--- terraform/consul_config/providers.tf
+++ terraform/consul_config/providers.tf
@@ -2,14 +2,15 @@ terraform {
   required_providers {
     consul = {
       source  = "hashicorp/consul"
-      version = "2.14.0"
+      version = "2.17.0"
     }
   }
 }
 
 provider "consul" {
-  address     = var.consul_address
-  datacenter  = var.datacenter
-  token       = var.token
-  scheme      = "https"
+  address        = var.consul_address
+  datacenter     = var.datacenter
+  token          = var.token
+  scheme         = "https"
+  insecure_https = var.insecure_https
 }
--- terraform/consul_config/variables.tf
+++ terraform/consul_config/variables.tf
@@ -1,3 +1,6 @@
 variable "consul_address" {}
 variable "datacenter" {}
 variable "token" {}
+variable "insecure_https" {
+    default = true
+}
