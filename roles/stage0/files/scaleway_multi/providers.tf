b8c3d6b88d8059c5726e7d26fb9770a02628702d
Aurélien Maury <aurelienmaury@users.noreply.github.com>
2023-04-06 15:01:44 +0200

    Online validation (#71)
    * make doc
    
    * renamed sidecar service
    
    * upgraded terraform scaleway provdier

--- roles/stage0/files/scaleway_multi/providers.tf
+++ roles/stage0/files/scaleway_multi/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     scaleway = {
       source  = "scaleway/scaleway"
-      version = "2.8.0"
+      version = "2.16.2"
     }
   }
   required_version = ">= 1.3"
